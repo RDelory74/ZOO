d19cbe71c55491add3b0da8a72fd874173670c36
RDelory74 <roro_spike@hotmail.com>
2024-07-02 16:18:58 +0200

    gitignore

---
+++ .gitignore/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'votre-nom/zoo',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'd3e39b75367336674292f973ac1d2d49e4f66be1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'votre-nom/zoo' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'd3e39b75367336674292f973ac1d2d49e4f66be1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
--- app.php
+++ app.php
@@ -3,3 +3,20 @@ require __DIR__ . '/vendor/autoload.php';
 
 
 // Here comes your code.
+
+abstract class Animal {
+    private string $name;
+    protected string $noise;
+
+
+    public function __construct(string $name)
+    {
+    
+    }
+    public function getName(): string {
+        return $this->name;
+    }
+    function getNoise(): string {
+        return $this->noise;
+    }
+}
